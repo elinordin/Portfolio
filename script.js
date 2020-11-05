9a6803cc3a10c432b1104066a6a14309abc65e68
elinordin <elin.evelina@live.se>
2020-11-05 21:04:13 +0100

    adding initial styling and on scroll animation to skills section

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 /*----------------------------VARIABLES----------------------------*/
+let bigGap = 72;
 let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
+let navbarPosition = navbar.offsetTop;
+let skillsPosition = document.getElementById("skills").offsetTop;
+let hardSkills = Array.from(document.getElementsByClassName("hard-progress-bar"));
+let softSkills = Array.from(document.getElementsByClassName("soft-progress-bar"));
 
 
 /*-------------------------EVENT LISTENERS-------------------------*/
 window.addEventListener("scroll", toggleStickyNavbar);
+window.addEventListener("scroll", animateProgress)
 
 
 /*--------------------------PROGRAM FLOW--------------------------*/
@@ -13,9 +18,22 @@ window.addEventListener("scroll", toggleStickyNavbar);
 
 /*----------------------------FUNCTIONS----------------------------*/
 function toggleStickyNavbar(){
-    if (window.pageYOffset >= sticky){
+    if (window.pageYOffset >= navbarPosition){
         navbar.classList.add("sticky");
     } else{
         navbar.classList.remove("sticky");
+    }
+}
+
+function animateProgress(){
+    if (window.pageYOffset >= skillsPosition - (bigGap * 2)){
+        hardSkills.forEach(hardSkill => {
+            hardSkill.style.display = "inline-block";
+            hardSkill.style.animation = "slideInLeft 1.5s";
+        });
+        softSkills.forEach(softSkill => {
+            softSkill.style.display = "inline-block";
+            softSkill.style.animation = "slideInRight 1.5s";
+        });
     }
 }
