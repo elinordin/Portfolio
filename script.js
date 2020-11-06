3e2ec9f8114dd3ae18aa9b72bc07f414df5a88b2
elinordin <elin.evelina@live.se>
2020-11-06 15:48:26 +0100

    adding infoboxes with links to projects in portfolio

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ let navbarPosition = navbar.offsetTop;
 let skillsPosition = document.getElementById("skills").offsetTop;
 let hardSkills = Array.from(document.getElementsByClassName("hard-progress-bar"));
 let softSkills = Array.from(document.getElementsByClassName("soft-progress-bar"));
+let projects = Array.from(document.getElementsByClassName("project"));
+let project;
 
 
 /*-------------------------EVENT LISTENERS-------------------------*/
 window.addEventListener("scroll", toggleStickyNavbar);
 window.addEventListener("scroll", animateProgress)
+projects.forEach(project => {
+    project.addEventListener("mouseenter", function(e){
+        project = e.target;
+        displayProjectInfo(project);
+    });
+    project.addEventListener("mouseleave", function(e){
+        project = e.target;
+        hideProjectInfo(project);
+    })
+})
+
 
 
 /*--------------------------PROGRAM FLOW--------------------------*/
@@ -36,4 +49,14 @@ function animateProgress(){
             softSkill.style.animation = "slideInRight 1.5s";
         });
     }
+}
+
+function displayProjectInfo(project){
+    project.firstElementChild.style.visibility = "visible";
+    project.firstElementChild.style.animation = "fadeInProjectInfo 1s";
+}
+
+function hideProjectInfo(project){
+    project.firstElementChild.style.animation = "none";
+    project.firstElementChild.style.visibility = "hidden";
 }
