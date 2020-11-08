e4276804d239ef3462a7529cddd4970cc08fd03e
elinordin <elin.evelina@live.se>
2020-11-08 22:15:08 +0100

    adding bigger font-size to landscape view, adding additional responsive styling

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let hardSkills = Array.from(document.getElementsByClassName("hard-progress-bar")
 let softSkills = Array.from(document.getElementsByClassName("soft-progress-bar"));
 let projects = Array.from(document.getElementsByClassName("project"));
 let project;
+let linkedInIcons = Array.from(document.getElementsByClassName("fa-linkedin-in"));
+let gitHubIcons = Array.from(document.getElementsByClassName("fa-github"));
+let mailIcons = Array.from(document.getElementsByClassName("fa-envelope"));
 
 
 /*-------------------------EVENT LISTENERS-------------------------*/
@@ -22,6 +25,11 @@ projects.forEach(project => {
         hideProjectInfo(project);
     })
 })
+if (window.innerHeight < window.innerWidth){ //If landscape-view
+    makeIconsBigger();
+} else if (window.innerHeight > window.innerWidth){
+    makeIconsSmaller();
+}
 
 
 
@@ -59,4 +67,28 @@ function displayProjectInfo(project){
 function hideProjectInfo(project){
     project.firstElementChild.style.animation = "none";
     project.firstElementChild.style.visibility = "hidden";
+}
+
+function makeIconsBigger(){
+    linkedInIcons.forEach(icon => {
+        icon.classList.add("fa-2x");
+    });
+    gitHubIcons.forEach(icon => {
+        icon.classList.add("fa-2x");
+    });
+    mailIcons.forEach(icon => {
+        icon.classList.add("fa-2x");
+    });
+}
+
+function makeIconsSmaller(){
+    linkedInIcons.forEach(icon => {
+        icon.classList.remove("fa-2x");
+    });
+    gitHubIcons.forEach(icon => {
+        icon.classList.remove("fa-2x");
+    });
+    mailIcons.forEach(icon => {
+        icon.classList.remove("fa-2x");
+    });
 }
