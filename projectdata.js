3f45c8fbac734c805da19d98ea3d5fd9a3e7bf74
elinordin <elin.evelina@live.se>
2020-11-11 15:47:15 +0100

    adding project data and applying that data to template on click of project on mobile or on read more a-tag

---
+++ projectdata.js
@@ -0,0 +1,47 @@
+const projectArray = 
+[
+    {
+        projectTitle: "My Personal Page",
+        brief: "The fundamentals module. Build a text-based personal website. Side-quest was to 1) include an image, background pattern, video or sound and 2) add a subpage linked from the main page and vice versa.",
+        goals: "To get the fundamental understanding and comfortability of writing HTML and CSS and challange myself to practice JavaScript on my own in preparation for the upcoming brief. My focus was to really understand the display and position properties.",
+        role: "This was a solo-project, which means I did everything on my own.",
+        challanges: "Since this was the first project I went in to completely on my own it was a challange to know exactly what to prioritize and where to focus. I also have a tendency to not take a break if I get stuck, which in most cases leads to frustration and the problem not being solved.",
+        solution: "I played around with many different things like animations, prototyping, fonts and basic JavaScript. It was a good first brief to get into the process of how to structure a project.",
+        learning1: "If(stuck){takeBreak()}",
+        learning2: "Use let or const instead of var",
+        learning3: "Write down your learnings to realize you are making progress",
+        learning4: "Make a list of must-haves and nice-to-haves to keep you focusing on the right things",
+        learning5: "To explain something to someone else is a great way of putting something in words and to simplify it to yourself",
+        link: "https://elinordin.github.io/My-Personal-Website/"
+    },
+    {
+        projectTitle: "Snake",
+        brief: "Make an interactive website using JavaScript, HTML and CSS. We were not allowed to use frameworks or external API's, and we did not have to support mobile.",
+        goals: "The most important thing for me was to practice to come up with the logic myself and break down a problem instead of following a turtorial. I also wanted to feel more comfortable and gain some confidence in general with programming.",
+        role: "This was a solo-project, which means I did everything on my own.",
+        challanges: "I had some issues with the logic of how to make the snake grow when it hit the food, to begin with I accidently created a stick-game instead of the snake game",
+        solution: "To reach my goal I started with making a flowchart with the game-logic and as challanges came along I developed that flowchart to break down problems further. I also developed my flowchart when I reached the point of adding some of my nice-to-haves like a highscore-board.",
+        learning1: "Confidence that I can break down a problem myself without a turtorial",
+        learning2: "Deeper understanding of objects, arrays and objects within arrays",
+        learning3: "The basics of canvas",
+        learning4: "Different event listeners (keydown, input and click)",
+        learning5: "I love flowcharts",
+        link: "https://elinordin.github.io/Snake/"
+    },
+    {
+        projectTitle: "ROTA - Habit Tracker",
+        brief: "Build a habit tracker using vanilla JavaScript.",
+        goals: "Learn how to work in groups with code, learn git, learn how to organize problems and break down tasks and also learn about storage.",
+        role: "Since this was our first team-brief, no client was invlolved and we all wanted to learn it all we decided to do everything together. Everything from prototyping to writing code is done together through mob-coding.",
+        challanges: "We decided to do everything together and mob-code the whole project we knew from the beginning that it wasn't going to be the most effective approach. We still feel like it was worth it since we learned so much from eachother, but since it took more time we did not have enough time to finish all of our ideas and going forward we will have to divide the tasks to be more effective.",
+        solution: "The goals were definetly reached by the whole team since we had a good group dynamic and we made sure that everybody was on board during the whole project. We also decided to design and code the project mobile-first since the user most likely will track the habits through their phone if they are doing it daily. Desktop-version was a nice-to-have.",
+        learning1: "Time management: count in time for unexpected and unpredictable problems",
+        learning2: "Git, GitHub and Figma",
+        learning3: "Mob-coding: dare to take coding initiatives",
+        learning4: "Local storage",
+        learning5: "How to use the console debugging tools",
+        link: "https://elinordin.github.io/Habit-tracker/"
+    }
+];
+
+export default projectArray;
--- script.js
+++ script.js
@@ -1,41 +1,76 @@
 /*----------------------------VARIABLES----------------------------*/
+const body = document.querySelector("body");
 let bigGap = 72;
-let navbar = document.getElementById("navbar");
+
+const navbar = document.getElementById("navbar");
 let navbarPosition = navbar.offsetTop;
+
 let skillsPosition = document.getElementById("skills").offsetTop;
-let hardSkills = Array.from(document.getElementsByClassName("hard-progress-bar"));
-let softSkills = Array.from(document.getElementsByClassName("soft-progress-bar"));
-let projects = Array.from(document.getElementsByClassName("project"));
-let project;
-let linkedInIcons = Array.from(document.getElementsByClassName("fa-linkedin-in"));
-let gitHubIcons = Array.from(document.getElementsByClassName("fa-github"));
-let mailIcons = Array.from(document.getElementsByClassName("fa-envelope"));
+const hardSkills = Array.from(document.getElementsByClassName("hard-progress-bar"));
+const softSkills = Array.from(document.getElementsByClassName("soft-progress-bar"));
+
+const linkedInIcons = Array.from(document.getElementsByClassName("fa-linkedin-in"));
+const gitHubIcons = Array.from(document.getElementsByClassName("fa-github"));
+const mailIcons = Array.from(document.getElementsByClassName("fa-envelope"));
+
+const projects = Array.from(document.getElementsByClassName("project"));
+const readMores = Array.from(document.getElementsByClassName("readMore"));
+let hoveredProject;
+let unHoveredProject;
+let clickedProject;
+let clickedReadMore;
+
+const popupTemplate = document.getElementById("popup-template").content;
+let templateCopy = document.importNode(popupTemplate, true);
+let numberInArray;
+
 
 
 /*-------------------------EVENT LISTENERS-------------------------*/
 window.addEventListener("scroll", toggleStickyNavbar);
 window.addEventListener("scroll", animateProgress)
-projects.forEach(project => {
-    project.addEventListener("mouseenter", function(e){
-        project = e.target;
-        displayProjectInfo(project);
-    });
-    project.addEventListener("mouseleave", function(e){
-        project = e.target;
-        hideProjectInfo(project);
-    })
-})
+
+
 if (window.innerHeight < window.innerWidth){ //If landscape-view
     makeIconsBigger();
-} else if (window.innerHeight > window.innerWidth){
+    projects.forEach(project => {
+        project.addEventListener("mouseenter", function(e){
+            hoveredProject = e.target;
+            displayProjectInfo(hoveredProject);
+        });
+        project.addEventListener("mouseleave", function(e){
+            unHoveredProject = e.target;
+            hideProjectInfo(unHoveredProject);
+        })
+    })
+} else if (window.innerHeight > window.innerWidth){ //If portrait-view
     makeIconsSmaller();
+    projects.forEach(project => {
+        project.addEventListener("click", function(e){
+            clickedProject = e.target;
+            console.log(clickedProject);
+            displayProjectPopup(clickedProject);
+        })
+    })
 }
+
+readMores.forEach(readMore => {
+    readMore.addEventListener("click", function(e){
+        clickedReadMore = e.target;
+        displayProjectPopup(clickedReadMore);
+    })
+});
+
+
 
 
 
 /*--------------------------PROGRAM FLOW--------------------------*/
+import projectArray from "/projectdata.js";
+console.log(projectArray);
+
 particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
+    console.log('callback - particles.js loaded');
   });
 
 
@@ -61,14 +96,14 @@ function animateProgress(){
     }
 }
 
-function displayProjectInfo(project){
-    project.firstElementChild.style.visibility = "visible";
-    project.firstElementChild.style.animation = "fadeInProjectInfo 1s";
+function displayProjectInfo(hoveredProject){
+    hoveredProject.firstElementChild.style.visibility = "visible";
+    hoveredProject.firstElementChild.style.animation = "fadeInProjectInfo 1s";
 }
 
-function hideProjectInfo(project){
-    project.firstElementChild.style.animation = "none";
-    project.firstElementChild.style.visibility = "hidden";
+function hideProjectInfo(unHoveredProject){
+    unHoveredProject.firstElementChild.style.animation = "none";
+    unHoveredProject.firstElementChild.style.visibility = "hidden";
 }
 
 function makeIconsBigger(){
@@ -93,4 +128,39 @@ function makeIconsSmaller(){
     mailIcons.forEach(icon => {
         icon.classList.remove("fa-2x");
     });
+}
+
+function displayProjectPopup(clickedProject){
+    getNumberInArrayOfClickedProject(clickedProject);
+
+    if (numberInArray === 0 || numberInArray === 1 || numberInArray === 2){
+        templateCopy.querySelector(".project-title").textContent = projectArray[numberInArray].projectTitle;
+        templateCopy.querySelector(".brief").textContent = projectArray[numberInArray].brief;
+        templateCopy.querySelector(".goals").textContent = projectArray[numberInArray].goals;
+        templateCopy.querySelector(".role").textContent = projectArray[numberInArray].role;
+        templateCopy.querySelector(".challanges").textContent = projectArray[numberInArray].challanges;
+        templateCopy.querySelector(".solution").textContent = projectArray[numberInArray].solution;
+        templateCopy.querySelector(".learning1").textContent = projectArray[numberInArray].learning1;
+        templateCopy.querySelector(".learning2").textContent = projectArray[numberInArray].learning2;
+        templateCopy.querySelector(".learning3").textContent = projectArray[numberInArray].learning3;
+        templateCopy.querySelector(".learning4").textContent = projectArray[numberInArray].learning4;
+        templateCopy.querySelector(".learning5").textContent = projectArray[numberInArray].learning5;
+        templateCopy.querySelector(".link").href = projectArray[numberInArray].link;
+        body.appendChild(templateCopy);
+    } else {
+        displayProjectInfo(clickedProject);
+    }
+}
+
+function getNumberInArrayOfClickedProject(clickedProject){
+    if (clickedProject.id === "personal" || clickedProject.id === "personal-read-more"){
+        numberInArray = 0;
+    } else if (clickedProject.id === "snake" || clickedProject.id ==="snake-read-more"){
+        numberInArray = 1;
+    } else if (clickedProject.id === "rota" || clickedProject.id==="rota-read-more"){
+        numberInArray = 2;
+    } else {
+        numberInArray = null;
+    }
+    return numberInArray;
 }
